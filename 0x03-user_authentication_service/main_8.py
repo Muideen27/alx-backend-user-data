6d871698e220d28e752ad4a578934b8098f3e70d
Muideen27 <ilorimuideen0000@gmail.com>
2023-09-18 06:32:37 +0000

    returning boolean on valid login

---
+++ 0x03-user_authentication_service/main_8.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Main file
+"""
+from auth import Auth
+
+email = 'bob@bob.com, bob1@bob.com'
+password = 'MyPwdOfBob'
+auth = Auth()
+
+auth.register_user(email, password)
+
+print(auth.valid_login(email, password))
+
+print(auth.valid_login(email, "WrongPwd"))
+
+print(auth.valid_login("unknown@email", password))
+
+print(auth.valid_login(email, password))
+
+print(auth.valid_login(email, password))
